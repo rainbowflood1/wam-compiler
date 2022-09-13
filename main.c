e21a7027adaa4d02b2ab7a0c17c4be54ac309ba8
rainbowflood1 <88160870+rainbowflood1@users.noreply.github.com>
2022-09-13 22:11:06 +0000

    made it to read files

--- main.c
+++ main.c
@@ -7,6 +7,19 @@ int main() {
   char ch;
   char print[500];
   char line[500];
+  char cmd[500];
+  char c[1000];
+  FILE *fptr;
+  if ((fptr = fopen("main.main", "r")) == NULL) {
+    printf("Error! File cannot be opened.");
+    // Program exits if the file pointer returns NULL.
+    exit(1);
+  }
+
+  // reads text until newline is encountered
+  fscanf(fptr, "%[^\n]", c);
+  printf("Data from the file:\n%s", c);
+  fclose(fptr);
 
   return 0;
 }
